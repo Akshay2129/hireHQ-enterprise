e81e9390a06a4890a523583c9161a2df2ae9c51b
Akshay Sangale <akshaysangale529@gmail.com>
2025-01-18 12:02:10 +0530

    development

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   AUTH_URL: "https://services-dev.thehirehqsandbox.com/auth/v1/api",
-
-}
+  BASE_URL: process.env['NG_APP_BASE_URL'] || "http://localhost:4200", // Fallback to localhost if undefined
+};
